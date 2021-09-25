b16ed98e1562fb69455c12f8e63f5654418715d0
ndokas99 <54295411+ndokas99@users.noreply.github.com>
2021-09-25 02:02:40 +0200

    added database

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from os import path
-from create_db import write
+
 
 app = Flask(__name__)
-app.debug = True
+app.debug = False
 app.config.update({
     "SECRET_KEY": 'H4HG75HF83H67BJ57HFUV5',
     "SQLALCHEMY_DATABASE_URI": 'sqlite:///universities.db',
@@ -62,14 +61,6 @@ def univ(name):
 def guide():
     return render_template("guidance.html")
 
-
-def create_database():
-    if not path.exists("universities.db"):
-        db.create_all(app=app)
-        write()
-
-
-create_database()
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
